4b64c0673f542fef74926aa4d53ab138d0a587b6
Sidney Andrews <sidney@seesharprun.net>
2016-06-29 10:09:14 -0400

    Homework Files

--- README.md
+++
@@ -1,2 +0,0 @@
-# homework-mod3
-Potential Solution for Module 3 Homework 
---
+++ app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app.component');
+var svc_roster_1 = require('./svc.roster');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [svc_roster_1.RosterService]);
+//# sourceMappingURL=main.js.map
